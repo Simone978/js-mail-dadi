75b06551d684a1229cb6a8e942d994d67cfb65af
Simone <simonelega.sl@gmail.com>
2020-01-07 14:27:47 +0100

    programma check mail completato

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,19 @@
-alert('ciao');
+
+
+var listaMail = ['pippo@gmail.com' , 'pluto@alice.it', 'paperino@hotmail.com', 'topolino@aruba.it'];
+
+var checkMail = prompt('inserisci la tua email');
+var mail = false;
+
+for (var i = 0; i < listaMail.length; i++) {
+  var lista = listaMail[i];
+  if(lista == checkMail){
+    mail = true;
+  }
+}
+
+if (mail == true){
+  alert('Complimenti, puoi accedere ai nostri contenuti esclusivi');
+}else{
+  alert('mi dispiace, email non riconosciuta');
+}
