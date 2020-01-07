1382b25af9f1a4a8a3e9afe035bea281719ceaf7
Simone <simonelega.sl@gmail.com>
2020-01-07 15:24:23 +0100

    Funzionamento gioco dadi completato

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,19 @@
-alert('ciao');
+
+var nomeGiocatore1 = prompt("inserisci il nome del primo giocatore");
+
+var giocatore = document.getElementById('umano');
+giocatore.addEventListener('click', function(){
+  var punteggioUmano= Math.floor(Math.random() * 6) + 1;
+  alert(nomeGiocatore1+' hai realizzato '+punteggioUmano+" punti");
+  var punteggioComputer= Math.floor(Math.random() * 6) + 1;
+  alert('Il computer ha realizzato '+punteggioComputer+" punti");
+
+  if(punteggioUmano > punteggioComputer) {
+    alert('complimenti, hai vinto');
+  }else if (punteggioUmano == punteggioComputer) {
+    alert('Pari. Nessun vincitore');
+  }else {
+    alert('hai perso');
+  }
+
+})
